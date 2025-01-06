89f08e26a26d2aa429b327f1f9a5a4073b2db9ff
rwtrecs <adolfo@rewrite.com.br>
2025-01-06 18:50:37 -0400

    class: 34

--- 05-primeiro-projeto-bloco-notas/notes/routes/web.php
+++ 05-primeiro-projeto-bloco-notas/notes/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MainController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,3 +11,5 @@ Route::get('/', function () {
 Route::get('/about', function () {
     echo 'About';
 });
+
+Route::get('/main', [MainController::class, 'index']);
