b6f3b5c29856cba0f915096ff3e3f67dabc28199
rwtrecs <adolfo@rewrite.com.br>
2025-01-07 19:51:20 -0400

    class: 39

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
@@ -6,7 +6,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index($value) {
-        return view('main', ['value' => $value]);
-    }
+
 }
--- 05-primeiro-projeto-bloco-notas/notes/routes/web.php
+++ 05-primeiro-projeto-bloco-notas/notes/routes/web.php
@@ -1,15 +1,4 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\MainController;
-
-Route::get('/', function () {
-    return view('main');
-});
-
-Route::get('/about', function () {
-    echo 'About';
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
