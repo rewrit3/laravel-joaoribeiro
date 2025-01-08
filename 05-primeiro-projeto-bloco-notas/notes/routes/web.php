16815ef89651e7bf9d69d6ebaac753d1181a2076
rwtrecs <adolfo@rewrite.com.br>
2025-01-07 20:01:06 -0400

    class: 40

--- 05-primeiro-projeto-bloco-notas/notes/routes/web.php
+++ 05-primeiro-projeto-bloco-notas/notes/routes/web.php
@@ -2,3 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
+
+// Auth
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
