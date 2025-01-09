ea719315d941aaf4a0355574c3aa35a42738d73d
rwtrecs <adolfo@rewrite.com.br>
2025-01-08 20:03:53 -0400

    class: 44

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/AuthController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/AuthController.php
@@ -13,9 +13,15 @@ class AuthController extends Controller
 
   public function loginSubmit(Request $request)
   {
-    echo $request->input('text_username');
-    echo '<br>';
-    echo $request->input('text_password');
+    $request->validate([
+      'text_username' => 'required',
+      'text_password' => 'required'
+    ]);
+
+    $username = $request->input('text_username');
+    $password = $request->input('text_password');
+
+    echo 'OK!';
   }
 
   public function logout()
