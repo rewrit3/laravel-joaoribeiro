4522d8b2c4681fe5e4bd219eeca003c361b45b22
rwtrecs <adolfo@rewrite.com.br>
2025-01-08 20:15:44 -0400

    class: 45

--- 05-primeiro-projeto-bloco-notas/notes/resources/views/login.blade.php
+++ 05-primeiro-projeto-bloco-notas/notes/resources/views/login.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <div class="container mt-5">
     <div class="row justify-content-center">
-      <div class="col-md-6 col-sm-8">
+      <div class="col-md-8 col-sm-8">
         <div class="card p-5">
           <div class="text-center p-3">
             <img src="assets/images/logo.png" alt="Notes logo">
@@ -12,16 +12,27 @@
             <div class="col-md-10 col-12">
               <form action="/login-submit" method="post">
                 @csrf
-                <div class="mb-3">
+                <div class="mb-2">
                   <label for="text_username" class="form-label">Username</label>
-                  <input type="text" class="form-control bg-dark text-info" name="text_username">
+                  <input type="text" class="form-control bg-dark text-info"
+                    name="text_username" value="{{ old('text_username') }}">
+                  @error('text_username')
+                    <div class="text-danger mt-1 mb-3">{{ $message }}</div>
+                  @enderror
                 </div>
+
                 <div class="mb-3">
                   <label for="text_password" class="form-label">Password</label>
-                  <input type="password" class="form-control bg-dark text-info" name="text_password">
+                  <input type="password" class="form-control bg-dark text-info"
+                    name="text_password" value="{{ old('text_password') }}">
+                  @error('text_password')
+                    <div class="text-danger mt-1 mb-3">{{ $message }}</div>
+                  @enderror
                 </div>
+
                 <div class="mb-3">
-                  <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
+                  <button type="submit"
+                    class="btn btn-secondary w-100">LOGIN</button>
                 </div>
               </form>
             </div>
@@ -30,16 +41,6 @@
           <div class="text-center text-secondary mt-3">
             <small>&copy;<?= date('Y') ?> - Notes</small>
           </div>
-
-          @if ($errors->any())
-            <div class="alert alert-danger mt-3">
-              <ul class="m-0">
-                @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-                @endforeach
-              </ul>
-            </div>
-          @endif
         </div>
       </div>
     </div>
