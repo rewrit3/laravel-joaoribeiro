ab76f46915be0c63fc042bd4f503bcc2a1d4d614
rwtrecs <adolfo@rewrite.com.br>
2025-01-08 20:47:25 -0400

    class: 47

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/AuthController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -30,7 +31,14 @@ class AuthController extends Controller
     $username = $request->input('text_username');
     $password = $request->input('text_password');
 
-    echo 'OK!';
+    try {
+      DB::connection()->getPdo();
+      echo 'Connection is OK!';
+    } catch (\PDOException $e) {
+      echo 'Connection failed: ' . $e->getMessage();
+    }
+
+    echo 'FIM';
   }
 
   public function logout()
