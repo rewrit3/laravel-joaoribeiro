9de8a65526b8034e364d1e9e25caeaebd40299cd
rwtrecs <adolfo@rewrite.com.br>
2025-01-18 17:28:35 -0400

    class: 54

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+  public function index()
+  {
+    echo 'main:index';
+  }
 
+  public function newNote()
+  {
+    echo 'main:newNote';
+  }
 }
--- 05-primeiro-projeto-bloco-notas/notes/routes/web.php
+++ 05-primeiro-projeto-bloco-notas/notes/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 
-// Auth
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login-submit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::middleware([CheckIsLogged::class])->group(function () {
+  Route::get('/', [MainController::class, 'index']);
+  Route::get('/new-note', [MainController::class, 'newNote']);
+  Route::get('/logout', [AuthController::class, 'logout']);
+});
