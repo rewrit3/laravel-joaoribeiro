dff217de0211d29e7c3d02803248499470f22c18
rwtrecs <adolfo@rewrite.com.br>
2025-01-18 18:24:05 -0400

    class: 59

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class MainController extends Controller
 {
   public function index()
   {
+    $id = session('user.id');
+    $user = User::find($id)->toArray();
+    $notes = User::find($id)->notes()->get()->toArray();
+
+    echo "<pre>";
+    var_dump($user);
+    var_dump($notes);
+    exit;
+
     return view('home');
   }
 
