14ae0c27489678b946ab95f73404e33c030dcff0
rwtrecs <adolfo@rewrite.com.br>
2025-01-18 17:36:15 -0400

    class: 55

--- 05-primeiro-projeto-bloco-notas/notes/routes/web.php
+++ 05-primeiro-projeto-bloco-notas/notes/routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/login-submit', [AuthController::class, 'loginSubmit']);
+Route::middleware([CheckIsNotLogged::class])->group(function () {
+  Route::get('/login', [AuthController::class, 'login']);
+  Route::post('/login-submit', [AuthController::class, 'loginSubmit']);
+});
 
 Route::middleware([CheckIsLogged::class])->group(function () {
   Route::get('/', [MainController::class, 'index']);
