2735d562a27249ee1f8bfd0a5f12548be1ffa534
rwtrecs <adolfo@rewrite.com.br>
2025-01-18 18:36:58 -0400

    class: 60

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
@@ -10,15 +10,9 @@ class MainController extends Controller
   public function index()
   {
     $id = session('user.id');
-    $user = User::find($id)->toArray();
     $notes = User::find($id)->notes()->get()->toArray();
 
-    echo "<pre>";
-    var_dump($user);
-    var_dump($notes);
-    exit;
-
-    return view('home');
+    return view('home', ['notes' => $notes]);
   }
 
   public function newNote()
