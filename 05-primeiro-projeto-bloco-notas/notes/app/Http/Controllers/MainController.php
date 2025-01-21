50b373a8940ca78d361c28ac59ffcd24ea36cb87
rwtrecs <adolfo@rewrite.com.br>
2025-01-21 19:36:18 -0400

    class: 63

--- 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
+++ 05-primeiro-projeto-bloco-notas/notes/app/Http/Controllers/MainController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
-use App\Models\User;
 use Illuminate\Contracts\Encryption\DecryptException;
+use App\Models\User;
 
 class MainController extends Controller
 {
@@ -24,19 +24,26 @@ class MainController extends Controller
 
   public function editNote($id)
   {
-    try {
-      $id = Crypt::decrypt($id);
-    } catch (DecryptException $e) {
-      return redirect()->route('home');
-    }
+    $id = $this->decryptID($id);
+
+    echo $id;
   }
 
   public function deleteNote($id)
+  {
+    $id = $this->decryptID($id);
+
+    echo $id;
+  }
+
+  private function decryptID($id)
   {
     try {
       $id = Crypt::decrypt($id);
     } catch (DecryptException $e) {
       return redirect()->route('home');
     }
+
+    return $id;
   }
 }
