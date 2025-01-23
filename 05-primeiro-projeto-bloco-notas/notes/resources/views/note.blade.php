a884809ef5762d6a81a97eceb4cdc965c744c745
rwtrecs <adolfo@rewrite.com.br>
2025-01-22 20:06:25 -0400

    class: 69

--- 05-primeiro-projeto-bloco-notas/notes/resources/views/note.blade.php
+++ 05-primeiro-projeto-bloco-notas/notes/resources/views/note.blade.php
@@ -4,6 +4,7 @@
       <div class="row">
         <div class="col">
           <h4 class="text-info">{{ $note['title'] }}</h4>
+
           <small class="text-secondary">
             <span class="opacity-75 me-2">
               Created at:
@@ -12,6 +13,18 @@
               {{ date('Y-m-d H:i:s', strtotime($note['created_at'])) }}
             </strong>
           </small>
+
+          @if ($note['created_at'] != $note['updated_at'])
+            <br>
+            <small class="text-secondary">
+              <span class="opacity-75 me-2">
+                Updated at:
+              </span>
+              <strong>
+                {{ date('Y-m-d H:i:s', strtotime($note['updated_at'])) }}
+              </strong>
+            </small>
+          @endif
         </div>
         <div class="col text-end">
           <a href="{{ route('edit', ['id' => Crypt::encrypt($note['id'])]) }}"
